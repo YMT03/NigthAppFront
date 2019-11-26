a98d636423016c70eaf890f24adb14be221f71cb
Ignacio Ramos <iramos.ee@gmail.com>
2019-11-26 13:24:18 -0300

    Habilidad de iniciar sesion

--- src/helper/HeaderUtil.js
+++ src/helper/HeaderUtil.js
@@ -2,7 +2,7 @@ let headers_ = new Headers
 headers_.set('Accept','application/json')
 headers_.set('Content-Type','application/json')
 export function getHeaders() {
-  return headers;
+  return headers_;
 }
 export function setCredentials(UserName, Password) {
   headers_.set('Authorization', 'Basic ' + btoa(UserName + ":" + Password))
